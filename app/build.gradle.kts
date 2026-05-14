da0bf6fafe53e54a1072860950337962a403a26f
Aleksey Karakuts <a.karakuts@barg.group>
2026-05-14 08:12:13 +0300

    Bump version to 1.1.0 (versionCode 10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "ru.akarakuts.tictactoe"
         minSdk = 24
         targetSdk = 36
-        versionCode = 9
-        versionName = "1.0.9"
+        versionCode = 10
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
